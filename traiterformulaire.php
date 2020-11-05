69d7d353c2b1930b9611d913cb2681a5590d927c
cnam <cnam@CNAM>
2020-11-05 09:56:01 -1000

    recup_d'infos

--- traiterformulaire.php
+++ traiterformulaire.php
@@ -1,40 +1,42 @@
 <?php 
     require('utils.php');
 
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $date = $_POST['date'];
-    $exigence = $_POST['exigence'];
-    $numero = $_POST['numero'];
-    $connex = GetConnection();
+  $nom = $_POST['nom'];
+   $prenom = $_POST['prenom'];
+   $email = $_POST['email'];
+   $date = $_POST['date'];
+   $exigence = $_POST['exigence'];
+   $numero = $_POST['numero'];
+   $prestaIds = $_POST['presta'];
+
+    //$nom = 'spidertest';
+    //$prenom = 'testdeouf';
+    //$email = '@mail';
+    //$date = date("Y-m-d");
+    //$exigence = 'chepa ';
+    //$numero ='52622626';
+    //$prestaIds = array('1','2','3');
+
+
+    $mysqli = GetConnection();
 
     $servername = "remotemysql.com";
     $username = "qgO0M364Or";
     $dbname = "qgO0M364Or";
     $password = "7Hyomgetg3";
 
-    $prestaIds = $_POST['presta'];
-    var_dump($prestaIds);
-
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-      }           
-  
-      catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-      }  
-  
-
-
     $sql = "INSERT INTO Devis (nom, prenom, email,date, exigence, numero) VALUES ('$nom','$prenom','$email','$date','$exigence', '$numero')";
 
+    $mysqli->query($sql);
+    $devisId = $mysqli->insert_id;
 
-    $conn->query($sql);
-    $devisId = $conn->insert_id;
     $sql = "";
+    foreach($prestaIds as $pid){
+      $sql .= "INSERT INTO DevisPrestataire (devisid,prestataireid) VALUES ($devisId,$pid);"; //concaténer si ne marche pas
+    }
 
+    $mysqli->multi_query($sql);
+ 
+    $mysqli->close();
 
     ?>
